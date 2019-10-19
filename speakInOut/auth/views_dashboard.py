85c889b40d373f679f9bd5a5d10a95dda9440d0b
Mishal Shah <shahmishal1998@gmail.com>
2019-10-20 00:13:50 +0530

    video recording implemented

--- speakInOut/auth/views_dashboard.py
+++ speakInOut/auth/views_dashboard.py
@@ -4,8 +4,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from speakInOut.helper import parse_session, authentication_check, register_user
 from auth.forms import LoginForm, AccountRegisterForm
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def dashboard_view(request):
     # Authentication check. Users currently logged in cannot view this page.
     authentication_result = authentication_check(request)
@@ -14,5 +16,7 @@ def dashboard_view(request):
     template_data = parse_session(request)
     # get template data from session
     template_data['profile'] = User.objects.get(username=request.user)
-    template_data['profile']
+    template_data['dashboard'] = True
+    if request.method == "POST":
+    	print(request.FILES)
     return render(request, 'dashboard.html', template_data)
