df9721e7a9ea71367f504f00a42203fe899c6132
Praveen Gupta <pvgupta24@gmail.com>
2019-10-20 07:19:48 +0530

    Integrates scripts with django

--- speakInOut/auth/views_dashboard.py
+++ speakInOut/auth/views_dashboard.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from speakInOut.helper import parse_session, authentication_check, register_user
@@ -9,6 +9,11 @@ from django.http import HttpResponse
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
+import threading
+import subprocess
+import os
+
+from eyeTrack import track
 
 @csrf_exempt
 def dashboard_view(request):
@@ -21,8 +26,45 @@ def dashboard_view(request):
     template_data['profile'] = User.objects.get(username=request.user)
     template_data['dashboard'] = True
     if request.method == "POST":
-        vd = request.FILES.get("audiovideo", None)
+        print(request.FILES)
+        print(len(request.FILES))
+        vd = request.FILES.get("audiovideo")
         print(type(vd))
-        path= default_storage.save('video/' + '123' + '.wav', ContentFile(vd.read()))
+        # print(default_storage)
+        path = default_storage.save('video/' + '123' + '.mp4', ContentFile(vd.read()))
+
+        # task = ThreadTask()
+        # task.save()
+        t = threading.Thread(target=longTask,args=[path])
+        t.setDaemon(True)
+        t.start()
         return HttpResponse(status=200)
     return render(request, 'dashboard.html', template_data)
+
+
+# def startThreadTask(request):
+#     task = ThreadTask()
+#     task.save()
+#     t = threading.Thread(target=longTask,args=[task.id])
+#     t.setDaemon(True)
+#     t.start()
+#     return JsonResponse({'id':task.id})
+
+# # Check status of long tash
+# def checkThreadTask(request,id):
+#     task = ThreadTask.objects.get(pk=id)
+#     return JsonResponse({'is_done':task.is_done})
+
+def longTask(video_path):
+    print("Analyzing ",video_path)    
+    track.analyzeFrames(video_path)
+    
+    print("Generating audio file")
+    audio_file_name = os.path.basename(video_path).split('.')[0] + '.wav'
+    command = "ffmpeg -i " + video_path + " -ab 160k -ac 2 -ar 44100 -vn audio/" + audio_file_name
+    subprocess.call(command, shell=True)
+
+    # task = ThreadTask.objects.get(pk=id)
+    # task.is_done = True
+    # task.save()
+    # print("Finished task",id)
